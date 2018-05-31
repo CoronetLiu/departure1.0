9210306ac1c19fff959520f576bd52f02b21d624
CoronetLiu <865753224@qq.com>
2018-05-31 10:31:55 +0800

    fixed Bug

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -21,16 +21,47 @@ class Home extends React.Component {
 
     render() {
         // console.log("render")
-        return (
-            <div id="warp">
-                <Menu content1="概览"/>
-                <div id="main-right">
-                    <div id="home">
-                        <img src="./assets/images/down.png" alt="图片" title="down.png"/>HOME
+
+        //判断登录状态
+        if(!$.cookie("ObmUser")){
+            layui.use('layer', function() {
+                var layer = layui.layer;
+                layer.alert("您尚未登录！", {
+                    icon: 7,
+                    skin: 'layui-layer-molv',
+                    anim: 4,
+                    area:['300px','180px'],
+                    move:false,
+                    shade:[1,"#fff"],
+                    closeBtn:false,
+                    success:function(){
+                        $(".layui-layer-content").css({
+                            height:"50px"
+                        });
+                    },
+                    btn: ["登录"],
+                    yes: function (index, layero) {  //第一个按钮的回调函数
+                        hashHistory.push({
+                            pathname: '/login'
+                        });
+                        layer.close(index)
+                    }
+                });
+            })
+            return (<div></div>);
+        }else{
+
+            return (
+                <div id="warp">
+                    <Menu content1="概览"/>
+                    <div id="main-right">
+                        <div id="home">
+                            <img src="./assets/images/down.png" alt="图片" title="down.png"/>HOME
+                        </div>
                     </div>
                 </div>
-            </div>
-        );
+            );
+        }
     }
 
     componentDidMount(){
